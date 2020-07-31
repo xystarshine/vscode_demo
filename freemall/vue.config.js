112a7b610897aaf9c5e268b009506bc3ebeea70f
xystarshine <2748083488@qq.com>
2020-07-31 17:04:33 +0800

    购物车实现

---
+++ freemall/vue.config.js
@@ -0,0 +1,14 @@
+module.exports={
+    publicPath:'',
+    devServer:{
+        host:'localhost',
+        port:8080,
+        proxy:{
+            '/mock':{
+            target:'http://localhost:8080',
+            ws:false,
+            changeOrigin:false
+            }
+        }
+    }
+}
